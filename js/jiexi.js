27fd24e2e7e68854d0753457563b3c7991142cd9
怡红公子 <845787297@qq.com>
2020-01-29 16:10:05 +0800

    解析接口找到源头

--- js/jiexi.js
+++ js/jiexi.js
@@ -3,6 +3,5 @@ $(function(){
 			$("iframe").css("display","block");
 			$("#readme").css("display","none");
 			$("iframe").attr("src",$(this).attr("data-url")+$("#input input").val());
-		
 	})
 })
