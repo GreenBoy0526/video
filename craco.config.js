3ce7fcfc25bc9e9a20bebea13f2701cbf6cfaa7b
GreenBoy <845787297@qq.com>
2025-01-02 15:19:17 +0800

    🐳 chore: 代理修改

--- craco.config.js
+++ craco.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api/videosourceapi": {
-        target: "https://api.wujinapi.me/api.php/provide/vod",
+        target: "http://localhost:3001/?url=https://api.wujinapi.me/api.php/provide/vod",
         changeOrigin: true,
         pathRewrite: {
           "^/api/videosourceapi": "",
